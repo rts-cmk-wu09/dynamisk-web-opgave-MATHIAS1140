b4914f31c906295e048460fc9a4ca56b67c452e3
MATHIAS1140 <U2420682@RTS-365.DK>
2023-02-13 14:13:32 +0100

    add hero og tilhørende inforkasse

--- js/index.js
+++ js/index.js
@@ -1,8 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // hero image start
+  const heroSection = document.querySelector(".hero");
+  const heroImg = document.createElement("img");
+  
+  heroImg.src = hero.image;
+  heroImg.alt = "dame bag en skranke";
+  heroSection.appendChild(heroImg);
+  // hero image slut
 
-    // slå dig løs her... 
+  // heroBox start
+  hero.headline = hero.headline.replace(
+    "save your time",
+    "<span>save your time</span>"
+  );
+  document.querySelector(".hero").innerHTML = `
+  <img src="${hero.image}">
+  <div class="heroBox">
+  <section class="h1Box">
+  <h1>${hero.headline}</h1>
+  </section>
+  <div class="p">
+  <p>
+  ${hero.copy}  
+  </p>
+  </div>
+  <button class="heroBox-Btn">
+  <img src="${hero.icon}">
+    <p>
+    Explore
+    </p>
+  </button>
+  </div>`;
+  // heroBox slut
 
-    //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
 
-}) // DOMContentLoaded slut
+
+  // heroText.className = "hej";
+});
